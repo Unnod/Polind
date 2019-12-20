3e926298284f1e76e3759f8a003f90628078334f
Innod <fomichev.roman@yandex.ru>
2019-12-20 20:49:33 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,43 @@ $(function(){
         $('.menu-list').slideToggle();
     });
 
- 
+    $('.header__items').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        // autoplay: true,
+        pauseOnHover:true,
+        responsive: [
+            {
+              breakpoint: 1411,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 580,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+        ]
+    });
+    // var header_slider = $('.header__items');
+    // $(function(){
+    //   header_slider.find('.slick-active:first').toggleClass('disable');
+    //   header_slider.find('.slick-active:last').toggleClass('disable');
+    // });
 
     var mixer = mixitup('.search__items');
 
