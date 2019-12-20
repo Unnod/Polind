d1145ec3f245eaa6db0e0997d94a1b0d1d0f094b
Innod <fomichev.roman@yandex.ru>
2019-12-20 15:47:24 +0300

    added adaptive menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,12 @@ $(function(){
 
     new WOW().init();
 
+    $('.menu__btn').on('click', function() {
+        $('.menu-list').slideToggle();
+    });
+
+ 
+
     var mixer = mixitup('.search__items');
 
     
